91205bcd9b104993cc4b32518a1f79493f553e6b
William Ashton <William@AshtonFam.org>
2021-09-27 15:14:03 -0400

    Extract the structs out to lib

--- src/main.rs
+++ src/main.rs
@@ -4,55 +4,9 @@ use std::fs::File;
 use std::net::{SocketAddr, TcpStream, ToSocketAddrs};
 use std::time::Duration;
 
-use serde::Deserialize;
 use serde_yaml;
 
-#[derive(Deserialize, Debug)]
-struct Services {
-    ssh: Option<bool>,
-    http: Option<bool>,
-    https: Option<bool>,
-    mariadb: Option<bool>,
-    postgresql: Option<bool>,
-    other: Option<HashMap<u16, bool>>,
-}
-
-impl Services {
-    fn to_port_checks(&self) -> HashMap<u16, bool> {
-        let mut ports = match &self.other {
-            Some(values) => values.clone(),
-            _ => HashMap::new(),
-        };
-
-        if let Some(enabled) = &self.ssh {
-            ports.insert(22, *enabled);
-        }
-
-        if let Some(enabled) = &self.http {
-            ports.insert(80, *enabled);
-        }
-
-        if let Some(enabled) = &self.https {
-            ports.insert(443, *enabled);
-        }
-
-        if let Some(enabled) = &self.mariadb {
-            ports.insert(3306, *enabled);
-        }
-
-        if let Some(enabled) = &self.postgresql {
-            ports.insert(5432, *enabled);
-        }
-
-        ports
-    }
-}
-
-#[derive(Deserialize, Debug)]
-struct Role {
-    services: Services,
-    servers: Vec<String>,
-}
+use concheck::Role;
 
 fn test_port(address: &SocketAddr, enabled: &bool) -> bool {
     match TcpStream::connect_timeout(address, Duration::from_secs(1)) {
@@ -88,19 +42,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut all_ports: HashSet<u16> = HashSet::new();
 
     for (_name, role) in &roles {
-        let port_checks = role.services.to_port_checks();
-        let ports: HashSet<u16> = port_checks.keys().cloned().collect();
-        all_ports = &all_ports | &ports;
+        all_ports = &all_ports | &role.ports();
     }
 
     println!("All ports: {:?}", all_ports);
 
     for (name, role) in &roles {
-        let port_checks = role.services.to_port_checks();
+        let port_checks = role.services().to_port_checks();
 
         println!("{}:", name);
 
-        for server in &role.servers {
+        for server in role.servers() {
             match check_server(server.to_string(), &port_checks) {
                 Ok(results) => {
                     println!("\t{} => {:?}", server, results)
