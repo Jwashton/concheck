dff9fd66df3df50c9bca95ca3d8a31480bbabceb
William Ashton <William@AshtonFam.org>
2021-09-27 18:07:31 -0400

    Format header

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,44 @@ fn check_server(address: IpAddr, port_checks: &HashMap<u16, bool>) -> HashMap<u1
         .collect()
 }
 
+fn format_header(ports: HashSet<u16>, longest_name: usize) -> String {
+    let mut ordered_ports = ports.iter().collect::<Vec<&u16>>();
+
+    ordered_ports.sort();
+
+    let header = ordered_ports
+        .iter()
+        .map(|port| port.to_string())
+        .collect::<Vec<String>>()
+        .join("\t");
+
+    format!(
+        "\t{: <15}\t{:width$}\t{}",
+        "-",
+        "-",
+        header,
+        width = longest_name
+    )
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open("./inventory.yml")?;
     let roles: HashMap<String, Role> = serde_yaml::from_reader(file)?;
 
-    let mut all_ports: HashSet<u16> = HashSet::new();
+    let all_ports = roles.iter().fold(HashSet::new(), |ports, (_name, role)| {
+        &ports | &role.ports()
+    });
+    let all_names = roles
+        .iter()
+        .map(|(_name, role)| role.servers())
+        .flatten()
+        .collect::<Vec<&String>>();
 
-    for (_name, role) in &roles {
-        all_ports = &all_ports | &role.ports();
-    }
+    let longest_name = all_names
+        .iter()
+        .max_by(|x, y| x.chars().count().cmp(&y.chars().count()));
 
-    println!("All ports: {:?}", all_ports);
+    println!("{}", format_header(all_ports, longest_name.unwrap().chars().count()));
 
     for (name, role) in &roles {
         let port_checks = role.services().to_port_checks();
